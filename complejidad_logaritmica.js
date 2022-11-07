290b71f21fd0ae0ed4f6bc528c54e7db8db9b47f
pipe-dev <pipe-dev>
2022-11-07 15:33:23 -0500

    feature: add logs for count time

--- complejidad_logaritmica.js
+++ complejidad_logaritmica.js
@@ -1,8 +1,9 @@
 /*
     O(log N) Complejidad cuadratica
 */
-
+console.time('binarySearch');
 function binarySearch(array, num, prefix = 0) {
+    if (array.length === 1) return array;
     let mIndex = Math.floor(array.length / 2);
     let mNumber = array[mIndex];
     let newArr = [];
@@ -19,3 +20,7 @@ function binarySearch(array, num, prefix = 0) {
     }
     return binarySearch(newArr, num, newPrefix);
 }
+binarySearch([1, 2, 3, 4, 5, 6], 10)
+console.timeEnd('binarySearch');
+
+
